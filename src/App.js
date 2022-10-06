15c195e4a1b6738d5766f17cb86a1595c53645f7
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-10-05 22:43:29 -0400

    create a list of todo elements, pass data to component, add new todo list item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useLayoutEffect } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const todos = [
+    { id: 1, text: "Go Shopping", done: false},    
+    { id: 2, text: "Drop kids at school", done: false},
+    { id: 3, text: "Buy groceries", done: false}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Todo List
+      </h1>
+      <TodoList todos={todos} />
     </div>
   );
 }
 
-export default App;
+function TodoList({todos}) {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  )}
+
+  function AddTodo() {
+    function handleAddTodo(event) {
+      event.preventDefault();
+      console.log(event.target.elements);
+    }
+    return(
+      <form onSubmit={handleAddTodo}>
+        <input placeholder='Add todo'/>
+        <button type='submit'> Submit </button>
+      </form>
+    )
+  }
